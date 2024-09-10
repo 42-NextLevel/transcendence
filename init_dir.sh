bf3c9a166f3b2bed69dd7fbd706c0a08293c0d70
seongwol <gbssw13@gmail.com>
2024-09-10 20:26:10 +0900

    🧱 디렉토리 자동완성 스크립트 추가

---
+++ init_dir.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ENV_FILE="srcs/.env"
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    # macOS
+    BASE_DIR="/Users/$USER/data"
+else
+    # Linux
+    BASE_DIR="/home/$USER/data"
+fi
+
+if ! grep -q "DATA_PATH=" srcs/.env; then
+	if [ -s srcs/.env ] && [ "$(tail -c 1 srcs/.env | wc -l)" -eq 0 ]; then
+    echo "" >> srcs/.env
+	fi
+    echo "DATA_PATH=$BASE_DIR" >> srcs/.env
+fi
+
+if [ "$2" == "--delete" ]; then
+    echo "Deleting volume..."
+    rm -rf "$BASE_DIR"
+    rm -rf "$BASE_DIR"
+    echo "Delete COMPLETE!!!"
+
+    if [ -f "$ENV_FILE" ]; then
+        sed -i '' '/^DATA_PATH=/d' "$ENV_FILE"
+    else
+        echo ".env file not found. Skipping DATA_PATH removal."
+    fi
+    exit 0
+fi
+
+if [ ! -d "$BASE_DIR" ]; then
+    echo "Making directory..."
+	mkdir -p $BASE_DIR/databases/
+    echo "MAKE COMPLETE!!!"
+fi
