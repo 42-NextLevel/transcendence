66e7fe3fb96915b2a235aebfd28a8df86a028bc0
nijesmik <bkybb@duck.com>
2024-10-22 18:21:29 +0900

    🧱 dev 브랜치 설정

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ft_transcendance
----
-init.
+
+## Usage
+
+1. `git submodule init`
+2. `git submodule update`
+3. `srcs/confidential/.env`를 `srcs/nginx/frontend`에 복사
+4. `make`
--- submodule_init.sh
+++ submodule_init.sh
@@ -1,5 +1,5 @@
 if [ ! -f srcs/confidential/.env ]; then
 	git submodule update --init --recursive --remote
-else
-	git submodule update --remote --merge
+# else
+# 	git submodule update --remote --merge
 fi
