37b6b02a89bbc3763e6c8a242f623fafdb3e0397
Seongwon Lee <seongwol@c1r1s6.42seoul.kr>
2024-11-05 20:53:24 +0900

    hotfix: <<<<<< text remove

--- srcs/init_dir.sh
+++ srcs/init_dir.sh
@@ -22,10 +22,7 @@ if [ "$1" == "--delete" ]; then
     echo "Deleting volume..."
     rm -rf "$DATA_PATH"
     rm -rf "$DATA_PATH"
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     echo "Delete COMPLETE!!!"
 
     if [ -f "$ENV_FILE" ]; then
